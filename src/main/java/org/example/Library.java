5a8b964ee33b48c52cf0488e763dcdd1ed7ac5a8
Nikhil2516 <nkbhot111@gmail.com>
2024-09-27 02:29:52 +0530

    Created Function for adding books

--- src/main/java/org/example/Library.java
+++ src/main/java/org/example/Library.java
@@ -1,5 +1,20 @@
 package org.example;
 
-public class Library {
+import java.util.ArrayList;
+import java.util.List;
 
+//Representing Library with books
+public class Library {
+    private List<Book> books;
+
+    // Constructor of Library with empty list of books
+    Library()
+    {
+        books = new ArrayList<>();
+    }
+
+    public void addBook(Book book)
+    {
+        books.add(book);
+    }
 }
