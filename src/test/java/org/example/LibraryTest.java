8a3c371680a5af495f5eae0d0718c164968ac5c4
Nikhil2516 <nkbhot111@gmail.com>
2024-09-27 02:24:58 +0530

    Test for adding books is created

--- src/test/java/org/example/LibraryTest.java
+++ src/test/java/org/example/LibraryTest.java
@@ -1,10 +1,29 @@
 package org.example;
 
+import org.example.Book;
+import org.example.Library;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class LibraryTest {
 
-    @org.junit.jupiter.api.BeforeEach
+    private Library library;
+    private Book book1;
+    private Book book2;
+    private Book book3;
+
+    @BeforeEach
     void setUp() {
+        library = new Library();
+        book1 = new Book("978-0134685991", "Effective Java", "Joshua Bloch", 2017);
+        book2 = new Book("978-0596009205", "Head First Java", "Kathy Sierra", 2005);
+        book3 = new Book("978-0132350884", "Clean Code", "Robert C. Martin", 2008);
+
+        library.addBook(book1);
+        library.addBook(book2);
+        library.addBook(book3);
     }
 }
